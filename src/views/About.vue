15f6e10793b7617270eefa5f8d117e5aa5e0f5cb
Adam <aausti23@myune.edu.au>
2019-10-20 22:05:36 +1100

    added some more projects

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,7 +5,7 @@
     <div class="content">
         <section class="has-text-left">
           <p>Australian Computer Science student studying at the University of New England. 
-          A veteran electrician with decades of experience in the industry seeking a new adventure 
+          A veteran electrician with 16 years of experience in the industry seeking a new adventure 
           and looking to transition into the software development sphere. I have an aptitude for 
           solving challenging problems in various coding languages and have actively immersed myself 
           in the development community in my spare time.
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -4,7 +4,7 @@
     <hr/>
     <div class="content">
         <section class="clearfix">
-            <h2>The Hive </h2>
+            <h2>The Hive</h2>
             <div class="has-text-left">
                 <a href="https://thehive.pw" target="_blank" class="aligned">
                 <img src="../assets/hive-preview.png">
@@ -17,7 +17,57 @@
             </div>
         </section>
         <hr />
-        
+        <section class="clearfix">
+            <h2>Project Management Quiz</h2>
+            <div class="has-text-left">
+                <a href="https://h3x.github.io/PM-quiz/" target="_blank" class="aligned">
+                <img src="../assets/quiz-preview.png">
+                </a>
+                <div><strong>Code:</strong> <a href="https://github.com/h3x/PM-quiz" target="_blank">https://github.com/h3x/PM-quiz</a><br />
+                <strong>Live site:</strong><a href="https://h3x.github.io/PM-quiz/" target="_blank">https://h3x.github.io/PM-quiz/</a><br/>
+                <strong>Description:</strong> <p>This quiz was made to help study the Project Management unit
+                    at university. I was able to gather fellow students in the same class to help with contributing
+                    questions and answers then open sourced the entire code base so others could benifit if they
+                    saw a use for the project i had created.
+                </p>
+                <strong>Technologies used:</strong> HTML, CSS, Javascript, JQuery
+                </div>
+            </div>
+        </section>
+        <hr />
+        <section class="clearfix">
+            <h2>BattleTanks</h2>
+            <div class="has-text-left">
+                <a href="https://github.com/h3x/battleTanks" target="_blank" class="aligned">
+                <img src="../assets/battletanks-preview.png">
+                </a>
+                <div><strong>Code:</strong> <a href="https://github.com/h3x/battleTanks" target="_blank">https://github.com/h3x/battleTanks</a><br />
+                <strong>Description:</strong> <p>BattleTanks was a group project worked on by myself and 2 other people. 
+                    BattleTanks is a multiplayer game played over a local network, or both players using the same keyboard.
+                    It was written in the processing environment. To play this game, you will need a copy of Processing with 
+                    the minim sound library installed ( available at <a href="https://processing.org/">https://processing.org/</a> )
+                </p>
+                <strong>Technologies used:</strong> Processing
+                </div>
+            </div>
+        </section>
+        <hr />
+        <section class="clearfix">
+            <h2>Portfolio</h2>
+            <div class="has-text-left">
+                <a href="https://adamjaustin.com" target="_blank" class="aligned">
+                <img src="../assets/portfolio-preview.png">
+                </a>
+                <div><strong>Code:</strong> <a href="https://github.com/h3x/portfolio" target="_blank">https://github.com/h3x/portfolio</a><br />
+                <strong>Live site:</strong><a href="https://adamjaustin.com" target="_blank">https://adamjaustin.com</a><br/>
+                <strong>Description:</strong> <p>This portfolio site. Designed and built for the purposes of showcaseing my experience, code
+                    and featured projects.
+                </p>
+                <strong>Technologies used:</strong> Vue, Node, HTML, CSS
+                </div>
+            </div>
+        </section>
+        <hr />
     </div>
   </div>
 </template>
@@ -27,4 +77,5 @@
     float: left;
     padding: 20px;
 }
+
 </style>
