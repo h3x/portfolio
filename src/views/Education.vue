0257a1c76be3d9f51ff3ac7bc61498a7aebb7079
Adam <aausti23@myune.edu.au>
2019-10-20 19:53:12 +1100

    moved some stuff around

--- src/views/Education.vue
+++ src/views/Education.vue
@@ -4,41 +4,43 @@
     <hr/>
     <div class="content">
         <section class="has-text-left">
-        <strong>University of New England</strong><br/>
-        Bachelor of Computer Science ( expected graduation October 2020 )<br /><br />
-        <strong>GPA:</strong> 6.75 / 7<br />
-        <strong>WAM:</strong> 88.25<br /><br />
-        <strong>Relevant Coursework:</strong>
-        <table>
-            <tr>
-                <td>Operating Systems </td>
-                <td>Artificial Intelligence </td>
-                <td>Software Project Management</td>
-            </tr>
-            <tr>
-                <td>Quantitative Skills With Applications ( Scala)</td>
-                <td>Discrete Mathematics </td>
-                <td>Object Oriented Programming (in java)</td>
-            </tr>
-            <tr>
-                <td>Data Structures and Algorithms (in C++)   </td>
-                <td>Calculus and Linear Algebra 1   </td>
-                <td>Web Programming</td>
-            </tr>
-            <tr>
-                <td>Advanced Web Programming</td>
-            </tr>
-        </table> 
+            <strong>University of New England</strong><br/>
+            Bachelor of Computer Science ( expected graduation October 2020 )<br /><br />
+            <strong>GPA:</strong> 6.75 / 7<br />
+            <strong>WAM:</strong> 88.25<br /><br />
+            <strong>Relevant Coursework:</strong>
+            <table>
+                <tr>
+                    <td>Operating Systems </td>
+                    <td>Artificial Intelligence </td>
+                </tr>
+                <tr>
+                    <td>Quantitative Skills With Applications ( Scala)</td>
+                    <td>Object Oriented Programming (in java)</td>
+                </tr>
+                <tr>
+                    <td>Data Structures and Algorithms (in C++) </td>
+                    <td>Calculus and Linear Algebra</td>
+                </tr>
+                <tr>
+                    <td>Advanced Web Programming</td>
+                    <td>Discrete Mathematics </td>
+                </tr>
+                <tr>
+                    <td>Software Project Management</td>
+                    <td>Web Programming</td>
+                </tr>
+            </table> 
         </section>
         <hr />
         <section class="has-text-left">
             <strong>TAFE NSW</strong><br/>
-            Completed coursework toward Electrical Engineering - Electronics ( December 2011 )<br />
+            Completed coursework toward Electrical Engineering - Electronics ( January 2009 - December 2011 )<br />
         </section>
         <hr />
         <section class="has-text-left">
             <strong>TAFE NSW</strong><br/>
-            Electrotechnology Electrician ( December 2006 )
+            Electrotechnology Electrician ( February 2002 - December 2006 )
         </section>
         <hr/>
     </div>
@@ -50,8 +52,14 @@ br{
     margin:0;
 }
 
-table,tr,td {
-    border: 0;
+table {
+    border: none;
+    border-collapse: collapse;
+    width:80%;
+}
+
+td,tr {
+    border: none;
 }
 </style>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    
+    <div class="home">
+    <h1>Adam Austin</h1>
+    <hr/>
+    <div class="content">
+        <section class="has-text-left clearfix">
+           
+         </section>
+    </div>
+    </div>
   </div>
 </template>
 
